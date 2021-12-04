de50dcd8fc2671e805216b1cdd6e29a0b19e1139
JKurenai <anysvph@gmail.com>
2021-12-03 19:02:45 -0500

    Initial commit

---
+++ final_proj.py
@@ -0,0 +1,6 @@
+API = '8HPsQUxBqappCs8ZBxkcypYLWZqaHr4FQnElbAlk'
+CME_URL = 'https://api.nasa.gov/DONKI/CMEAnalysis?'
+FLR_URL = 'https://api.nasa.gov/DONKI/FLR?'
+
+start_date = '2021-01-01'
+end_date = '2021-06-30'
